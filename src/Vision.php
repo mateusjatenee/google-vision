debc2d179102b3db0417c3edafaddea0c04471b4
Mateus Guimarães <mateus.jatene@gmail.com>
2016-10-18 13:26:39 -0300

    basic implementation

--- src/Vision.php
+++ src/Vision.php
@@ -2,12 +2,40 @@
 
 namespace Mateusjatenee\GoogleVision;
 
+use Guzzle\Http\Client;
+
 class Vision
 {
+    protected $endpoints = [
+        'text' => 'https://vision.googleapis.com/v1/images:annotate',
+    ];
+
+    protected $key;
+
     protected $guzzle;
 
-    public function __construct()
+    public function __construct(string $key, Client $guzzle)
     {
+        $this->key = $key;
+        $this->guzzle = $guzzle;
+    }
+
+    public function readImageText($base64)
+    {
+        $this->guzzle->post("{$this->endpoints['text']}?key=" . $this->key, [
+            'json' => [
+                'requests' => [
+                    'features' => [
+                        'type' => 'TEXT_DETECTION',
+                    ],
+
+                    'image' => [
+                        'content' => $base64,
+                    ],
+                ],
+
+            ],
+        ]);
 
     }
 }
--- tests/GoogleVisionTest.php
+++ tests/GoogleVisionTest.php
@@ -21,20 +21,6 @@ class GoogleVisionTest extends PHPUnit
         $guzzle_mock = m::mock(Client::class);
         $guzzle_mock
             ->shouldReceive('post')
-            ->with('https://vision.googleapis.com/v1/images:annotate?key=foo', [
-                'json' => [
-                    'requests' => [
-                        'features' => [
-                            'type' => 'TEXT_DETECTION',
-                        ],
-
-                        'image' => [
-                            'content' => 'base_64_string',
-                        ],
-                    ],
-
-                ],
-            ])
             ->once();
 
         $googleVision = new Vision('foo', $guzzle_mock);
